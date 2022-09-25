bc3830d1bb11c34aff0ced58ca54885a0e036762
Bishal Giri <bishal0922@gmail.com>
2022-09-25 11:29:43 -0500

    Adding the components

--- text-editor/src/app/app.module.ts
+++ text-editor/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RichTextEditorModule, ToolbarService, LinkService, ImageService, HtmlEditor} from '@syncfusion/ej2-angular-richtexteditor';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RichTextEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
